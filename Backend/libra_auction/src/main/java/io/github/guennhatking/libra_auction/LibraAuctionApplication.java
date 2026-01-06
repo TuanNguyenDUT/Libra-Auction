396f6e4c138954af9546df14e3dcf272ec79a8fc
Guen Nhat King <GuenNhatKing@gmail.com>
2026-01-06 13:18:10 +0700

    Initialize

---
+++ Backend/libra_auction/src/main/java/io/github/guennhatking/libra_auction/LibraAuctionApplication.java
@@ -0,0 +1,13 @@
+package io.github.guennhatking.libra_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraAuctionApplication.class, args);
+	}
+
+}
---
+++ Backend/libra_auction/src/test/java/io/github/guennhatking/libra_auction/LibraAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guennhatking.libra_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
